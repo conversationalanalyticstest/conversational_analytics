6e88c736a1defab61437265cced1d069d099fba5
conversationalanalyticstest <conversationalanalyticstest@outlook.com>
2026-09-04 10:42:01 +0200

    fix: revertir NET_SALES a neto (gross - discount), rompio a proposito en Escenario 3 (#5)
    Co-authored-by: Daniel <dsanchezramos@deloitte.es>

--- snowflake/004_semantic_view.sql
+++ snowflake/004_semantic_view.sql
@@ -114,7 +114,7 @@ CREATE OR ALTER SEMANTIC VIEW SV_PHARMA_SALES
     SALE.DISCOUNT AS SUM(SALE.DISCOUNT_AMOUNT)
       WITH SYNONYMS ('discount', 'discounts', 'discount amount')
       COMMENT = 'Total discount granted, in euros.',
-    SALE.NET_SALES AS SUM(SALE.GROSS_AMOUNT)
+    SALE.NET_SALES AS SUM(SALE.NET_AMOUNT)
       WITH SYNONYMS ('sales', 'net sales', 'revenue')
       COMMENT = 'Total net sales in euros (gross minus discount). Default business metric for unqualified "sales".',
     SALE.AVG_NET_SALES AS AVG(SALE.NET_AMOUNT)
